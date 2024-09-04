7436526d7ceef6c2d08c94ac8254bcaa41f76299
Roman <romansrgvch@outlook.com>
2024-09-04 11:28:51 +0300

    finish user

--- planner-backend/src/auth/decorators/user.decorator.ts
+++ planner-backend/src/auth/decorators/user.decorator.ts
@@ -1,10 +1,10 @@
+import type { User } from '@/../prisma/generated/client'
 import { createParamDecorator, ExecutionContext } from '@nestjs/common'
-import { User } from 'prisma/generated/client'
 
 export const CurrentUser = createParamDecorator(
 	(data: keyof User, ctx: ExecutionContext) => {
 		const request = ctx.switchToHttp().getRequest()
-		const user = request.User
+		const user = request.user
 
 		return data ? user[data] : user
 	}
